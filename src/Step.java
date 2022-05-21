ca76d4d70dbee5e353433ab700449c3e4dae85af
Your Name <you@example.com>
2022-05-21 19:36:23 +0600

    First commit

---
+++ src/Step.java
@@ -0,0 +1,12 @@
+public class Step {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 20;
+        int mile = 1;
+        int bonus = x / y;
+        {
+            System.out.println("Бонусные мили=" + x / y);
+        }
+
+    }
+}
