49e012cdf1d26226e1753db3b9d5069c9c2aeec9
Your Name <you@example.com>
2022-05-28 15:35:34 +0600

    Change Stepjava

--- src/Step.java
+++ src/Step.java
@@ -1,8 +1,10 @@
 public class Step {
     public static void main(String[] args) {
         int startMoney = 20;
-        int finishMoney = 100;
-        int bonusMile = finishMoney / startMoney = 100 / 20;
-        System.out.println("Бонусные мили=" + 100 / 20);
+        int finishMoney = 140;
+
+
+        int bonusMile = finishMoney / startMoney;
+        System.out.println("Бонусные мили=" + bonusMile);
     }
 }
